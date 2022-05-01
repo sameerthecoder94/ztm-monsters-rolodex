91767fa656c7b1f378dfb9e6d9b5b57fa8cac343
sameerthecoder94 <sameerthecoder@gmail.com>
2022-05-01 11:13:30 -0400

    States & Shallow Merge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: { firstName: 'Monsters', lastName: 'Something' },
+    };
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            Hi {this.state.name.firstName} {this.state.name.lastName}{' '}
+          </p>
+          <button
+            onClick={() => {
+              this.setState({
+                name: { firstName: 'Monsters', lastName: 'Rolodex' },
+              });
+            }}
+          >
+            Change Name
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
